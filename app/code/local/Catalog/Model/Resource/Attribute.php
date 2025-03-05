0371ba78b502a556b4c9c8b376d0651931eadcb0
nisarg1782 <nisargt1782@gmail.com>
2025-03-05 10:23:35 +0530

    05-03-25

--- app/code/local/Catalog/Model/Resource/Attribute.php
+++ app/code/local/Catalog/Model/Resource/Attribute.php
@@ -1,9 +1,8 @@
 <?php
 class Catalog_Model_Resource_Attribute extends Core_Model_Resource_Abstract
 {
-    PUBLIC FUNCTION __construct()
+    public function __construct()
     {
-        $this->init("catalog_attribute","attribute_id");
+        $this->init("catalog_attribute", "attribute_id");
     }
 }
-?>
