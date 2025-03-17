31d222b299ec1d212777b679b0096445dc271cba
nisarg1782 <nisargt1782@gmail.com>
2025-03-17 12:11:13 +0530

    cart update

--- app/code/local/Checkout/Block/Address/Index.php
+++ app/code/local/Checkout/Block/Address/Index.php
@@ -1,18 +1,18 @@
 <?php
 class Checkout_Block_Address_Index extends Core_Block_Template
 {
-    public function getAddress($cartid)
+    public function getAddress()
     {
         // $cartid=Mage::getSingleton("checkout/session")->getCart()->getCartId();
         // Mage::log($cartid);
         // $data=Mage::getModel("checkout/cart_address")->getCollection()->addFieldToFilter(
         //     "cart_id",["="=>$cartid]
         // )->getData();
-        // if(!empty($data))
-        // {
-        //     return $data;
-        // }
-        // // mage::log($data);
+        // // if(!empty($data))
+        // // {
+        // //     return $data;
+        // // }
+        // mage::log($data);
         // die;
         
     }
